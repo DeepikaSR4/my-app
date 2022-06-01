455f99fa57b408c2a32d35b7a90a7aa55f36d4c4
DeepikaSR4 <deepikasanathanan1234@gmail.com>
2022-06-01 23:16:27 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import React from 'react';
 import { BpkCode } from 'bpk-component-code';
 import BpkButton from 'bpk-component-button';
 import BpkText from 'bpk-component-text';
-import React, { Component } from 'react';
-import BpkCalendar from 'bpk-component-calendar';
-import BpkInput, { INPUT_TYPES } from 'bpk-component-input';
-import format from 'date-fns/format';
 import STYLES from './App.scss';
 
 const c = className => STYLES[className] || 'UNKNOWN';
-
 const App = () => (
   <div className={c('App')}>
     <header className={c('App__header')}>
@@ -17,63 +12,6 @@ const App = () => (
         <BpkText tagName="h1" textStyle="xxl" className={c('App__heading')}>Flight schedule</BpkText>
       </div>
     </header>
-const formatDateFull = date => format(date, 'EEEE, do MMMM yyyy');
-const formatMonth = date => format(date, 'MMMM yyyy');
-const daysOfWeek = [
-  {
-    name: 'Sunday',
-    nameAbbr: 'Sun',
-    index: 0,
-    isWeekend: true,
-  },
-  // ...
-];
-
-export default class App extends Component {
-  constructor () {
-    super();
-
-    this.state = {
-      selectionConfiguration: {
-        type: CALENDAR_SELECTION_TYPE.single,
-        date: null,
-        }
-    };
-  }
-
-  handleDateSelect = (date) => {
-    this.setState({
-      selectionConfiguration: {
-        type: this.props.selectionConfiguration.type,
-        date: date,
-      },
-    });
-  }
-
-  render () {
-    return (
-      <div>
-        <BpkInput
-          id='dateInput'
-          type={INPUT_TYPES.text}
-          name='date'
-          value={(this.state.selectionConfiguration.date || '').toString()}
-          placeholder='Departure date'
-        />
-        <BpkCalendar
-          id='calendar'
-          onDateSelect={this.handleDateSelect}
-          formatMonth={formatMonth}
-          formatDateFull={formatDateFull}
-          daysOfWeek={daysOfWeek}
-          weekStartsOn={1}
-          changeMonthLabel="Change month"
-          nextMonthLabel="Next month"
-          previousMonthLabel="Previous month"
-          selectionConfiguration={this.state.selectionConfiguration}
-        />
-      </div>
-    )
     <main className={c('App__main')}>
       <BpkText tagName="p" className={c('App__text')}>
         To get started, edit <BpkCode>src/App.jsx</BpkCode> and save to reload.
